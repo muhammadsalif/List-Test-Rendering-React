21db90ad7a63386be77d980e760c7e806ef1785d
Muhammad Salif <muhammad.salif@yahoo.com>
2021-01-15 18:12:11 +0500

    list testing ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+
+
+var handleDelete = (index) => {
+  console.log("index: ", index);
+}
+
+
+
 
 function App() {
+
+  var [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    function apiCall() {
+      axios.get("http://localhost:5000/sensors?chipsetId=5ff749ac122adf1b68dc6b12")
+        .then((_response) => {
+
+          setData(_response.data)
+
+        });
+    }
+    apiCall()
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    (data && data.length) ? data.map((eachItem, index) => {
+
+      return <div >
+         _id: {eachItem._id}
+         name: {eachItem.name}
+
+        <button onClick={() => { handleDelete(eachItem._id) }}>Delete</button>
+      </div>
+
+
+    }) : <div>Loading</div>
   );
 }
 
