f7a1122bf2eb9b4b3e0aff71ffa664e9c3875e96
Muhammad Salif <muhammad.salif@yahoo.com>
2021-01-25 00:13:19 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-var handleDelete = (index) => {
-  console.log("index: ", index);
-}
 
 
 
 
 function App() {
-
+  var [anchorEl, setAnchorEl] = useState(null);
   var [data, setData] = useState([]);
+
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log(event.currentTarget);
+    // console.log(event.currentTarget.id);
+    // console.log(Object.keys(event.currentTarget));
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  var handleDelete = (index) => {
+    console.log("index: ", anchorEl.id);
+  }
 
 
   useEffect(() => {
@@ -33,11 +49,41 @@ function App() {
   return (
     (data && data.length) ? data.map((eachItem, index) => {
 
-      return <div >
-         _id: {eachItem._id}
+      return <div key={index} >
+        _id: {eachItem._id}
          name: {eachItem.name}
 
+
         <button onClick={() => { handleDelete(eachItem._id) }}>Delete</button>
+        <button
+          // color="secondary"
+          // variant="outlined"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          id={index}
+        >
+          open menu        </button>
+
+
+        <Menu
+
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            style={{ display: "flex", justifyContent: "space-between", alignItems: "center", color: "red" }}
+            onClick={() => { handleDelete(eachItem._id) }}
+          >
+            Delete
+             </MenuItem>
+
+        </Menu>
+
+
       </div>
 
 
